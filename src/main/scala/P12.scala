ef1544bb0466fab7c59a861b537c3c81884e2908
Tory Law <git@torypages.com>
2015-09-06 19:47:48 -0400

    initial commit

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -1,7 +1,18 @@
 package org.p99.scala
 
+/*
+ * Decode a run-length encoded list.
+ * Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+ * Example:
+ *
+ * scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+ * res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+ *
+ * Apparently there is a function List.make that does something similar to my for loop
+ */
+
 object P12 {
-
-  // TODO
-
+  def decode(ls: List[(Int, Any)]): List[Any] = {
+    ls.flatMap(x => (for (i <- List.range(0, x._1)) yield x._2))
+  }
 }
--- src/test/scala/P12Spec.scala
+++ src/test/scala/P12Spec.scala
@@ -5,6 +5,10 @@ import org.scalatest._
 class P12Spec extends UnitSpec {
 
   // TODO
-
+  "decode" should "expand the list of lists from P10" in {
+    val actual = P12.decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+    val expected = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    assert(actual == expected)
+  }
 }
 
